22a015c92e00269b6389de92d3da6193140aae6c
Dmitry Dzhus <mail@sphinx.net.ru>
2007-09-21 22:18:47 +0400

    1.29: Simpson formula

---
+++ simple/1.29.scm
@@ -0,0 +1,20 @@
+(define (cube x) (* x x x))
+
+(define (sum term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (+ result (term a)))))
+  (iter a 0))
+
+(define (integrate f a b n)
+  (define h (/ (- b a) n))
+  (define (next-k a k) (+ a (* k h)))
+  (define (prev-k a k) (- a (* k h)))
+  (define (next a) (next-k a 1))
+  (define (next-2 a) (next-k a 2))
+  (* (/ h 3) (+ (f a)
+                (* 4 (sum f (next a) next-2 (prev-k b 1)))
+                (* 2 (sum f (next-2 a) next-2 (prev-k b 2)))
+                (f b)))
+)
