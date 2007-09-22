45b8f967be52ba382bee105cce7724e5202dc8c7
Dmitry Dzhus <mail@sphinx.net.ru>
2007-09-22 19:21:28 +0400

    Added iterative version for 1.43

--- simple/1.43.scm
+++ simple/1.43.scm
@@ -1,7 +1,14 @@
 (define (compose f g)
   (lambda (x) (f (g x))))
 
-(define (times f n)
-  (if (= n 0)
-      (lambda (x) x)
-      (compose f (times f (- n 1)))))
+(define (times-recursive f n)
+  (if (= n 1)
+      f
+      (compose f (times-recursive f (- n 1)))))
+
+(define (times-iterative f n)
+  (define (compose-step g k)
+    (if (= k 1)
+        g
+        (compose-step (compose f g) (- k 1))))
+  (compose-step f n))
