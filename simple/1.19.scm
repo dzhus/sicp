b3953f3ab52637db2da4d99d8db507e46b9bc15f
Dmitry Dzhus <mail@sphinx.net.ru>
2007-09-28 23:33:28 +0400

    2.15 — explanation about different results with interval arithmetics

--- simple/1.19.scm
+++ simple/1.19.scm
@@ -10,8 +10,8 @@
                    (+ (* p p) (* q q))
                    (+ (* 2 p q) (* q q))
                    (/ count 2)))
-         (else (fib-iter (+ (* b q) (* a q) (* a p))
-                         (+ (* b p) (* a q))
-                         p
-                         q
-                         (- count 1)))))
+        (else (fib-iter (+ (* b q) (* a q) (* a p))
+                        (+ (* b p) (* a q))
+                        p
+                        q
+                        (- count 1)))))
