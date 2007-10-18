36e4a7fb36188c9b3d9cfd3deb9a3126c11d93e4
Dmitry Dzhus <mail@sphinx.net.ru>
2007-10-18 22:50:22 +0400

    Horizontal flipping, rotations. Also special `beside` version to deal with non-global drawing.

---
+++ painting/2.50.scm
@@ -0,0 +1,60 @@
+;; Load `frame-coord-map` definition
+(load "2.49.scm")
+
+(define (transform-painter painter origin corner1 corner2)
+  (lambda (frame)
+    (let ((m (frame-coord-map frame)))
+      (let ((new-origin (m origin)))
+        (painter
+         (make-frame new-origin
+                     (sub-vect (m corner1) new-origin)
+                     (sub-vect (m corner2) new-origin)))))))
+
+(define (shrink-to-origin painter)
+  (transform-painter painter
+                     (make-vect 0.0 0.0)
+                     (make-vect 0.5 0.0)
+                     (make-vect 0.0 0.5)))
+
+(define (beside painter1 painter2)
+  (let ((split-point (make-vect 0.5 0.0)))
+    (let ((paint-left
+           (transform-painter painter1
+                              (make-vect 0.0 0.0)
+                              split-point
+                              (make-vect 0.0 1.0)))
+          (paint-right
+           (transform-painter painter2
+                              split-point
+                              (make-vect 1.0 0.0)
+                              (make-vect 0.5 1.0))))
+      ;; Not the same as in SICP to comply with function-oriented
+      ;; drawing (see comments in `cairo.scm`)
+      (lambda (frame)
+        (lambda (picture)
+          ((paint-left frame) ((paint-right frame) picture)))))))
+
+(define (flip-horiz painter)
+  (transform-painter painter
+                     (make-vect 1 0)
+                     (make-vect 0 0)
+                     (make-vect 1 1)))
+
+(define (ccw-90 painter)
+  (transform-painter painter
+                     (make-vect 1 0)
+                     (make-vect 1 1)
+                     (make-vect 0 0)))
+
+(define (ccw-180 painter)
+  (transform-painter painter
+                     (make-vect 1 1)
+                     (make-vect 0 1)
+                     (make-vect 1 0)))
+
+
+(define (ccw-270 painter)
+  (transform-painter painter
+                     (make-vect 0 1)
+                     (make-vect 0 0)
+                     (make-vect 1 1)))
