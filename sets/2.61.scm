d94a8ec83d7efac4268f2fc1603ee0dd16f9adc8
Dmitry Dzhus <mail@sphinx.net.ru>
2007-12-24 10:03:38 +0300

    Added iterative version of `adjoin-set` introduced in previous commit.

--- sets/2.61.scm
+++ sets/2.61.scm
@@ -17,9 +17,23 @@
               ((> x1 x2)
                (intersection-set set1 (cdr set2)))))))
 
+;; Recursive
 (define (adjoin-set x set)
   (let ((y (car set)))
     (cond ((null? set) x)
           ((= x y) set)
           ((< x y) (cons x set))
           (else (cons y (adjoin-set x (cdr set)))))))
+
+;; Iterative
+(define (adjoin-set x set)
+  (define (adjoin-step preceding following)
+    (if (null? following)
+        (append preceding (list x))
+        (let ((y (car following)))
+          (cond
+           ((= x y) (append preceding following))
+           ((< x y) (append preceding (cons x following)))
+           (else (adjoin-step (append preceding (list y))
+                             (cdr following)))))))
+  (adjoin-step '() set))
