1396267f0e0c08567473a0a9cf440a30a4675de2
Dmitry Dzhus <mail@sphinx.net.ru>
2008-04-27 22:31:04 +0400

    A more generic version of `lookup` in 2.66

--- sets/2.66.scm
+++ sets/2.66.scm
@@ -7,17 +7,48 @@
 (define (right-branch tree)
   (caddr tree))
 
-(define (lookup given-key tree)
-  (cond ((null? tree) false)
-        ((equal? given-key (key (entry tree)))
-         (entry tree))
+(define (lookup given-key set)
+  (cond ((null? set) #f)
+        ((match? given-key (key (first-record set)))
+         (first-record set))
         (else
-         (if (< given-key (key (entry tree)))
-             (lookup given-key (left-branch tree))
-             (lookup given-key (right-branch tree))))))
+         (lookup given-key (drop-unmatched-record set given-key)))))
+
+;; Tree version
+(define first-record entry)
 
 (define (key record)
   (car record))
+
+(define match? equal?)
+
+(define (drop-unmatched-record tree given-key)
+  (if (< given-key (key (entry tree)))
+      (left-branch tree)
+      (right-branch tree)))
+
+;;;                      Abstraction levels:
+
+;;;                       +---------------+
+;;;                       |    lookup     |
+;;;                       +------+--------+
+;;;                              |
+;;;                  +-----------+--------------+
+;;;                  |         match?           |
+;;;                  |          key             |
+;;;                  |      first-record        |
+;;;                  |   drop-unmatched-record  |
+;;;                  +---------------+----------+
+;;;                                  |
+;;;                                 -+--
+;;;              +-----------------/    \+--------------+
+;;;              |    :TREES:      |     |   :LISTS:    |
+;;;              |     equal?      |     |    equal?    |
+;;;              |      car        |     |     car      |
+;;;              |     entry       |     |     car      |
+;;;              |  |left-branch   |     |     cdr      |
+;;;              |  |right-branch  |     |              |
+;;;              +-----------------+     +--------------+
 
 ;; > (define db (list->tree '((1 Chuck) (2 Bob) (3 Dylan) (4 Helga) (5 John) (6 Spike))))
 ;; > (lookup 5 db)
