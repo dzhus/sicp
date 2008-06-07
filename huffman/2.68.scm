65e048e672d8d1cad9c7612a93cd2380f351a318
Dmitry Dzhus <mail@sphinx.net.ru>
2008-06-07 17:18:13 +0400

    2.67&2.68 (both in `2.68.scm`) — Huffman encoding basics

---
+++ huffman/2.68.scm
@@ -0,0 +1,83 @@
+(define (make-leaf symbol weight)
+  (list 'leaf symbol weight))
+
+(define (leaf? object)
+  (eq? (car object) 'leaf))
+
+(define (symbol-leaf x) (cadr x))
+
+(define (weight-leaf x) (caddr x))
+
+(define (make-code-tree left right)
+  (list left
+        right
+        (append (symbols left) (symbols right))
+        (+ (weight left) (weight right))))
+
+(define (symbols tree)
+  (if (leaf? tree)
+      (list (symbol-leaf tree))
+      (caddr tree)))
+
+(define (weight tree)
+  (if (leaf? tree)
+      (weight-leaf tree)
+      (cadddr tree)))
+
+(define (left-branch tree) (car tree))
+
+(define (right-branch tree) (cadr tree))
+
+(define (decode bits tree)
+  (define (decode-1 bits current-branch)
+    (if (null? bits)
+        '()
+        (let ((next-branch
+               (choose-branch (car bits) current-branch)))
+          (if (leaf? next-branch)
+              (cons (symbol-leaf next-branch)
+                    (decode-1 (cdr bits) tree))
+              (decode-1 (cdr bits) next-branch)))))
+  (decode-1 bits tree))
+
+(define (choose-branch bit branch)
+  (cond ((= bit 0) (left-branch branch))
+        ((= bit 1) (right-branch branch))
+        (else (error "BAD DATA"))))
+
+(define (encode message tree)
+  (if (null? message)
+      '()
+      (append (encode-symbol (car message) tree)
+              (encode (cdr message) tree))))
+
+(define element-of-set? memq)
+
+(define (symbol-in-tree? symbol tree)
+  (element-of-set? symbol (symbols tree)))
+
+(define (encode-symbol symbol tree)
+  (if (not (symbol-in-tree? symbol tree))
+      (error "COULD NOT ENCODE SYMBOL")
+      (if (leaf? tree)
+          '()
+          (if (symbol-in-tree? symbol (left-branch tree))
+              (cons '0 (encode-symbol symbol (left-branch tree)))
+              (cons '1 (encode-symbol symbol (right-branch tree)))))))
+
+(define sample-tree
+  (make-code-tree (make-leaf 'A 4)
+                  (make-code-tree
+                   (make-leaf 'B 2)
+                   (make-code-tree (make-leaf 'D 1)
+                                   (make-leaf 'C 1)))))
+
+(define sample-phrase '(A D A B B C A))
+
+(define sample-message '(0 1 1 0 0 1 0 1 0 1 1 1 0))
+
+; > (decode sample-message sample-tree)
+; (A D A B B C A)
+
+; > (encode sample-phrase sample-tree)
+; (0 1 1 0 0 1 0 1 0 1 1 1 0)
