1a15e4e441286c43005ba7fd2b24067405392b1f
Dmitry Dzhus <dima@sphinx.net.ru>
2008-07-06 20:51:00 +0400

    Type coercions (2.82)

---
+++ generic-ops/2.82.scm
@@ -0,0 +1,69 @@
+(define (get-coercion type1 type2)
+  (if (eq? type1 type2)
+      (lambda (t) t)
+      (get type1 type2)))
+
+;; True if list does not contain any #f
+(define (non-false-list? list)
+  (if (null? list)
+      #t
+      (and (car list)
+           (non-false-list? (cdr list)))))
+
+(define (get-coercions target-type types)
+  (map (lambda (type) 
+         (get-coercion type target-type))
+       types))
+
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    ;; Return a list of coerced arguments
+    (define (coerce-args coercions)
+      (map apply coercions
+           (map list args)))
+    
+    ;; Apply operation to coerced arguments
+    (define (apply-coercing coercions)
+      (let* ((coerced-args (coerce-args coercions))
+             (new-type-tags (map type-tag coerced-args))
+             (proc (get op new-type-tags)))
+        (apply proc (map contents coerced-args))))
+    
+    ;; Try to apply operation to arguments coerced to the head of
+    ;; `target-types` list. If it's impossible, try the same with next
+    ;; type in list.
+    (define (try-apply-generic-coercing target-types)
+      (if (null? target-types)
+          (error "NOT IMPLEMENTED FOR GIVEN TYPES")
+          (let ((coercions (get-coercions (car target-types)
+                                          type-tags)))
+            (if (non-false-list? coercions)
+                (apply-coercing coercions)
+                (try-apply-generic-coercing (cdr target-types))))))
+    
+    (let ((proc (get op type-tags)))
+      (if proc
+          (apply proc (map contents args))
+          (try-apply-generic-coercing type-tags)))))
+  
+
+;;; Example:
+;;
+;; Extend complex package with some ternary operation:
+;; > (define (complex-ternary z1 z2 z3) (make-complex-from-real-imag (+ (real-part z1) (real-part z2)) (- (imag-part z2) (imag-part z3))))
+;; > (put 'ternary '(complex complex complex) complex-ternary)
+;; > (define (ternary x y z) (apply-generic 'ternary x y z))
+;; > (define z1 (make-complex-from-real-imag 1 2))
+;; > (define z2 (make-complex-from-real-imag 3 0))
+;; > (define z3 (make-complex-from-real-imag 8 -7))
+;; > (ternary z1 z2 z3)
+;; (complex rectangular 4 . 7)
+;;
+;; Add a scheme-number->complex coercion:
+;; > (put 'scheme-number 'complex (lambda (x) (make-complex-from-real-imag (contents x) 0)))
+;;
+;; Try to call ternary with mixed arguments:
+;; > (ternary z1 3 z3)
+;; (complex rectangular 4 . 7)
+;; > (ternary 1 2 z1)
+;; (complex rectangular 3 . -2)
