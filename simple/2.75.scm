9b5534709a63dfa2c8afc49fef2030cad834ae30
Dmitry Dzhus <dima@sphinx.net.ru>
2008-07-06 13:53:01 +0400

    `make-from-mag-ang` in message-passing style (2.75)

---
+++ simple/2.75.scm
@@ -0,0 +1,15 @@
+(define (make-from-mag-ang mag ang)
+  (lambda (op)
+    (cond ((eq? op 'real-part) (* mag (cos ang)))
+          ((eq? op 'imag-part) (* mag (sin ang)))
+          ((eq? op 'magnitude) mag)
+          ((eq? op 'angle) ang)
+          (else
+           (error "UNKNOWN OPERATION")))))
+
+(define (apply-generic op arg) (arg op))
+
+(define (real-part z) (apply-generic 'real-part z))
+(define (imag-part z) (apply-generic 'imag-part z))
+(define (magnitude z) (apply-generic 'magnitude z))
+(define (angle z) (apply-generic 'angle z))
